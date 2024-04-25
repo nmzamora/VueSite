7079d2dee1cc6e5a84b87c9cdcf6dd7eb48c33ce
Nicholas Zamora <nickzmra@gmail.com>
2024-04-24 21:22:07 -0500

    Added tab card to Research page

--- src/views/ResearchView.vue
+++ src/views/ResearchView.vue
@@ -1,11 +1,101 @@
 <template>
-  <div class="container">
-    <h3>Research</h3>
-    <ul>
-      <li>
-        <h5><a href="/Research/RoboticSolarTrackingSystem">Text</a></h5>
-      </li>
-    </ul>
+  <div class="container px-5 mt-2 mb-4">
+    <div class="d-flex align-items-start card container flex-row g-0 w-50 mx-auto">
+      <div
+        class="nav flex-column nav-pills me-3 card-header"
+        id="v-pills-tab"
+        role="tablist"
+        aria-orientation="vertical"
+      >
+        <button
+          class="nav-link active"
+          id="v-pills-home-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#v-pills-home"
+          type="button"
+          role="tab"
+          aria-controls="v-pills-home"
+          aria-selected="true"
+        >
+          Home
+        </button>
+        <button
+          class="nav-link"
+          id="v-pills-profile-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#v-pills-profile"
+          type="button"
+          role="tab"
+          aria-controls="v-pills-profile"
+          aria-selected="false"
+        >
+          Profile
+        </button>
+        <button
+          class="nav-link"
+          id="v-pills-messages-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#v-pills-messages"
+          type="button"
+          role="tab"
+          aria-controls="v-pills-messages"
+          aria-selected="false"
+        >
+          Messages
+        </button>
+        <button
+          class="nav-link"
+          id="v-pills-settings-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#v-pills-settings"
+          type="button"
+          role="tab"
+          aria-controls="v-pills-settings"
+          aria-selected="false"
+        >
+          Settings
+        </button>
+      </div>
+      <div class="tab-content" id="v-pills-tabContent">
+        <div
+          class="tab-pane fade show active"
+          id="v-pills-home"
+          role="tabpanel"
+          aria-labelledby="v-pills-home-tab"
+          tabindex="0"
+        >
+          <h4 class="mt-2">Text</h4>
+        </div>
+        <div
+          class="tab-pane fade"
+          id="v-pills-profile"
+          role="tabpanel"
+          aria-labelledby="v-pills-profile-tab"
+          tabindex="0"
+        >
+          <h4 class="mt-2">Text</h4>
+        </div>
+        <div
+          class="tab-pane fade"
+          id="v-pills-messages"
+          role="tabpanel"
+          aria-labelledby="v-pills-messages-tab"
+          tabindex="0"
+        >
+          <h4 class="mt-2">Text</h4>
+        </div>
+        <div
+          class="tab-pane fade"
+          id="v-pills-settings"
+          role="tabpanel"
+          aria-labelledby="v-pills-settings-tab"
+          tabindex="0"
+        >
+          <h4 class="mt-2">Text</h4>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <style scoped></style>
+<script setup lang="ts"></script>
