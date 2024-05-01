feb81f4a0ab277762de679b7bbe1343be97452d8
Nicholas Zamora <nickzmra@gmail.com>
2024-04-30 21:34:33 -0500

    Fix for scroll up button

--- src/components/ScrollToTopView.vue
+++ src/components/ScrollToTopView.vue
@@ -1,16 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const isVisible = ref(false)
 
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' })
-}
+onMounted(() => {
+  const scrollToTopBtn = document.getElementById('scrollToTopBtn')
+  window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY
+    if (scrollY > 50) {
+      // Adjusted scroll threshold
+      // Show button after 100px scroll
+      isVisible.value = true
+    } else {
+      isVisible.value = false
+    }
+  })
+
+  scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  })
+})
 </script>
 
 <template>
   <div>
     <button
+      v-if="isVisible"
       id="scrollToTopBtn"
       class="scroll-to-top-btn"
       :class="{ visible: isVisible }"
